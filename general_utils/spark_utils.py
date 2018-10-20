c58e39d3828ffffb6e824549737bd0692c76d6f4
kinglavi <klavior@gmail.com>
2018-10-20 23:16:27 +0300

    - updated spark session name

--- general_utils/spark_utils.py
+++ general_utils/spark_utils.py
@@ -2,6 +2,6 @@ from pyspark.sql import SparkSession
 
 
 def get_spark_context():
-    spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
+    spark = SparkSession.builder.appName("MySpark").getOrCreate()
     sc = spark.sparkContext
     return spark, sc
